a66b2ce6a72fcc96c546bb4140eb1945a85d8ced
ktyoung <ktyoung0507@gmail.com>
2023-01-02 18:56:49 +0900

    Union Type, Any, Unknown

--- index.ts
+++ index.ts
@@ -1,30 +1,48 @@
-// 타입스크립트 기본 타입(primitive types) 정리
+// Union Type, Any, Unknown
 
-// 1. 변수 타입 지정
+// 1. Union Type : 2개 이상의 타입을 합친 새로운 타입 → |(or) 기호 사용
+//  1-1. 일반 변수
+let var1 :(number | string | boolean) = 123; // → 용도에 따라 가변적인 타입을 가짐
+//  1-2. array / object 자료형
+let arr1 :(string | number)[] = [1, '2', 3];
+let obj1 :{a : string | number} = { a : '123' };
+
+// 2. Any Type : 모든 자료형을 허용 → 타입 해제 문법... 일반 JS 변수로 만들고 싶을 때 사용
+let any1 :any;
+any1 = 123;
+any1 = [];
+any1 = '123';
+
+// 3. Unknown Type : 모든 자료형을 허용
+// → any 보다 안전한 이유는? 
+let unk1 :unknown;
+unk1 = 123;
+unk1 = {};
+// let unk2 :string = unk1; // → unk2에 string 타입이 지정되어 있으므로 에러 발생
+
+// (참고 1) ※※※ 타입스크립트는 간단한 수학 연산도 타입이 일치해야 함 ※※※
+// unk1 - 1; → unknown은 number 타입이 아님
+
+let var2 :string | number;
+// var2 + 1; → string 타입 + 1(가능), number 타입 + 1(가능), string|number 타입 + 1(불가)
+
+let unk3 :unknown = 1;
+// unk3 - 1; → 값이 숫자여도 unknown 타입이므로 연산 불가
+
+// (참고 2) 타입 지정 연습
 let user :string = 'kim';
-// user = 123; → 타입 변경 시 에러 발생
-let age :number = 22;
-let marriage :boolean = true;
-// undefined, null 타입도 있음
+let age :undefined | number = undefined;
+let married :boolean = false;
+let john :(string | undefined | number | boolean)[] = [user, age, married];
 
-// 2. array 타입 지정
-let arr :string[] = ['kim', 'park'];
-
-// 3. object 타입 지정
-let obj :{member1 : string, member2 : string} = { member1 : 'kim', member2 : 'park' };
-
-// 타입스크립트는 타입 지정을 자동으로 함
-let var1 = 'hello'; // → string 타입으로 자동 지정
-// var1 = 100; → 에러 발생
-
-let favMusic :{ artist : string, title : string } = { artist : '윤하', title : '오르트 구름' };
-
-let project :{
-    member : string[], 
-    days : number, 
-    stated : boolean
+let school : {
+    score : (number | boolean)[],
+    teacher : string,
+    friend : string | string[]
 } = {
-    member : ['kim', 'park'],
-    days : 30,
-    stated : true
+    score : [100, 97, 84],
+    teacher : 'Phil',
+    friend : 'John'
 }
+school.score[4] = false;
+school.friend = ['Lee', school.teacher];
