846d975a7595e1c0eec02d51a954aa7b36bd0bd1
ktyoung <ktyoung0507@gmail.com>
2023-01-03 20:33:50 +0900

    type alias (타입변수)

--- index.ts
+++ index.ts
@@ -1,73 +1,52 @@
-// Narrowing & Assertion
+// type alias (타입변수)
 
-// 1. Type이 하나로 확정되지 않았을 경우 Type Narrowing(타입을 하나로 확정) 사용
-//  1-1. 대표적인 Narrowing 방법은? typeof 연산자
-function myFunc1(x :number | string) {
-    if (typeof x === "string" ) {
-        console.log(x);
-    } else {
-        return x + 1;
-    }
-}
-//  1-2. (주의점) if문은 else, else if문으로 마무리하는 것이 에러 방지로 좋음
-function myFunc2(x :number | string) {
-    let arr1 :number[] = [];
-    // Narrowing
-    if (typeof x === "number") {
-        arr1[0] = x;
-    } else {
-        console.log(x);
-    }
-}
+// 1. 타입 변수 생성 및 사용 → 타입 변수는 영어 대문자로 시작하여 일반 변수와 차이점을 두기
+//  1-1. 일반 변수
+type MyType1 = string | number | undefined;
+let var1 :MyType1 = "Hello!";
+//  1-2. object 자료형
+type MyType2 = { name : string, age : number };
+let obj1 = { name : 'kim', age : 20 };
 
-// 2. Type Assertion (타입 덮어쓰기)
-//  2-1. 용도는? 
-//   2-1-1. Narrowing할 때 사용(타입을 a에서 b로 변경하는 것이 아님!)
-//    (불가능 예) let var1 :number = 1; 
-//               var1 as string;
-//   2-1-2. 무슨 타입이 들어올 지 100% 확실할 때 사용
+// 2. type 키워드 여러개를 extend
+//  2-1. OR 연산자를 이용해서 Union type
+type Name = string;
+type Age = number;
+type Person = Name | Age;
+//  2-2. & 기호를 쓴다면 object 안의 두개의 속성을 extend
+type PositionX = { x :number };
+type PositionY = { y :number };
+type Coordinate = PositionX & PositionY; // == { x :number, y :number }
+let position :Coordinate = { x : 10, y : 20 };
 
-function myFunc3(x :number | string) {
-    let arr2 :number[] = [];
-    // Assertion
-    arr2[0] = x as number; // → 왼쪽 변수를 number로 덮어씀
-}
+// (참고 1) const 변수는 변경할 수 없는 변수
+const city1 = 'seoul';
+// 1. const 변수는 등호로 재할당을 막는 역할... object 수정은 자유롭게 가능
+const city2 = { city : 'anyang' };
+city2.city = 'busan';
+// 2. 타입스크립트는 object 자료 수정을 방지할 수 있다 → readonly 키워드
+type MyType3 = { readonly city :string };
+const city3 :MyType3 = { city : 'daegu' };
+// city3.city = 'goseong'; → readonly 키워드 때문에 에러 발생
 
-// (참고 1) Narrowing 판정 문법 → 현재 변수의 타입을 특정할 수만 있다면 Narrowing 판정 문법임
-// 1. typeof 변수명
-// 2. 속성명 in 오브젝트자료
-// 3. 인스턴스 instanceof 부모
+// (참고 2) 같은 이름의 type 변수는 재정의 불가능
+type MyType4 = string;
+// type MyType4 = number; → 에러 발생
 
-// (참고 2) Narrowing 연습
-function toNumber(arr3 :(number | string)[]) {
-    let newArr :number[] = []
-
-    arr3.forEach((b)=>{
-        if (typeof b === 'string') {
-            newArr.push(parseFloat(b))
-        } else {
-            newArr.push(b)
-        }
-      })
-
-    return newArr;
-}
-console.log( toNumber([123, '3']) );
-
-let teacher1 = { subject : "math" };
-let teacher2 = { subject : ["science", "english"] };
-let teacher3 = { hello : "hi" };
-
-function teacher(sub : { subject : string | string[] } ) {
-    if (typeof sub.subject === "string") {
-        return sub.subject;
-    } else if (Array.isArray(sub.subject)) {
-        return sub.subject[sub.subject.length - 1];
-    } else {
-        return "타입 에러!"
-    }
+// (참고 3) type alias 연습
+type Type1 = {
+    color? :string,
+    size : number,
+    readonly position : number[]
 }
 
-console.log( teacher( teacher1 ) );
-console.log( teacher( teacher2 ) );
-// console.log( teacher( teacher3 ) );
+type Type2 = {
+    name :string,
+    phone :number,
+    email :string
+}
+
+type Type3 = {
+    teen :boolean;
+}
+type NewType = Type2 & Type3
