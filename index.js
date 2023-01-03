a5e770e260840519464a6836d1def2ca94b30331
ktyoung <ktyoung0507@gmail.com>
2023-01-03 21:32:32 +0900

    함수, methods type alias

--- index.js
+++ index.js
@@ -1,68 +1,49 @@
-// Narrowing & Assertion
-// 1. Type이 하나로 확정되지 않았을 경우 Type Narrowing(타입을 하나로 확정) 사용
-//  1-1. 대표적인 Narrowing 방법은? typeof 연산자
-function myFunc1(x) {
-    if (typeof x === "string") {
-        console.log(x);
-    }
-    else {
-        return x + 1;
-    }
+// 함수와 methods에 type alias 지정
+// 1. 일반적인 타입 지정
+function example1(a) {
+    return 1;
 }
-//  1-2. (주의점) if문은 else, else if문으로 마무리하는 것이 에러 방지로 좋음
-function myFunc2(x) {
-    var arr1 = [];
-    // Narrowing
-    if (typeof x === "number") {
-        arr1[0] = x;
-    }
-    else {
-        console.log(x);
-    }
+//  2-2. 함수표현식에만 type alias 사용 가능
+var myFunction = function example2(a) {
+    return 1;
+};
+// 3. object 자료형 안에 함수를 생성할 수 있음... 타입 지정은 어떻게?
+var info1 = {
+    name: 'kim',
+    plusOne: function (a) {
+        return a + 1;
+    },
+    changeName: function () { },
+};
+// (참고 1) 콜백함수?
+function func1(a) {
+    a();
 }
-// 2. Type Assertion (타입 덮어쓰기)
-//  2-1. 용도는? 
-//   2-1-1. Narrowing할 때 사용(타입을 a에서 b로 변경하는 것이 아님!)
-//    (불가능 예) let var1 :number = 1; 
-//               var1 as string;
-//   2-1-2. 무슨 타입이 들어올 지 100% 확실할 때 사용
-function myFunc3(x) {
-    var arr2 = [];
-    // Assertion
-    arr2[0] = x; // → 왼쪽 변수를 number로 덮어씀
+function func2() {
 }
-// (참고 1) Narrowing 판정 문법 → 현재 변수의 타입을 특정할 수만 있다면 Narrowing 판정 문법임
-// 1. typeof 변수명
-// 2. 속성명 in 오브젝트자료
-// 3. 인스턴스 instanceof 부모
-// (참고 2) Narrowing 연습
-function toNumber(arr3) {
-    var newArr = [];
-    arr3.forEach(function (b) {
-        if (typeof b === 'string') {
-            newArr.push(parseFloat(b));
-        }
-        else {
-            newArr.push(b);
-        }
-    });
-    return newArr;
+func1(func2); // func2가 콜백함수
+var info2 = {
+    name: 'kim',
+    plusOne: function (a) {
+        return a + 1;
+    },
+    changeName: function () { },
+};
+var cutZero = function cutZero(str) {
+    // 특정 문자 제거하기
+    var newStr = str.replace(/^0+/, '');
+    return newStr;
+};
+var removeDash = function removeDash(str) {
+    // 특정 문자 모두 제거하기(정규식 사용)
+    var newStr = str.replace(/-/g, ''); // → '/'는 정규식의 시작과 끝을 의미, 'g'는 Global Search(전역탐색)을 의미
+    return newStr;
+};
+console.log(cutZero('00123130'));
+console.log(removeDash('----123---zzz------123'));
+function myFunction2(a, func1, func2) {
+    var result = func1(a);
+    var result2 = func2(result);
+    console.log(result2);
 }
-console.log(toNumber([123, '3']));
-var teacher1 = { subject: "math" };
-var teacher2 = { subject: ["science", "english"] };
-var teacher3 = { hello: "hi" };
-function teacher(sub) {
-    if (typeof sub.subject === "string") {
-        return sub.subject;
-    }
-    else if (Array.isArray(sub.subject)) {
-        return sub.subject[sub.subject.length - 1];
-    }
-    else {
-        return "타입 에러!";
-    }
-}
-console.log(teacher(teacher1));
-console.log(teacher(teacher2));
-// console.log( teacher( teacher3 ) );
+myFunction2('010-1111-2222', cutZero, removeDash);
--- index.ts
+++ index.ts
@@ -1,48 +1,75 @@
-// Literal Types
+// 함수와 methods에 type alias 지정
 
-// (참고 1) Literal Types 장점?
-// 1. 변수의 타입을 더 엄격하게 관리할 수 있음
-// 2. 자동완성
-
-// 1. 더 엄격한 타입 지정 (Literal Types)
-//  1-1. 일반 변수
-let userName :'kim'; // → userName 변수에는 'kim'만 들어올 수 있음
-// userName = 123;
-let info :'26' | 'anyang';
-// info = '10';
-
-//  1-2. 함수
-function example1(x :'hello') :(1 | 0) {
+// 1. 일반적인 타입 지정
+function example1(a :string) :number {
     return 1;
 }
-function example2(x :('가위' | '바위' | '보')) :('가위' | '바위' | '보')[] {
-    return ['가위'];
+
+// 2. type alias에 함수 type 저장해 사용
+//  2-1. 함수 타입은 () => {}의 형태로
+type FunctionType1 = (a :string) => number;
+//  2-2. 함수표현식에만 type alias 사용 가능
+let myFunction :FunctionType1 = function example2(a) {
+    return 1;
 }
 
-// 2. Literal Types은 const 변수와 유사하게 사용 가능
-//  2-1. Literal Types은 const 변수의 상위 호환 → 한 번에 여러개의 상수를 저장하여 사용할 수 있음!
-const var1 = "hi";
-let var2 :"hi" | "hello";
-
-// 3. Literal Types의 문제점?
-var content = {
-    name :'kim',
-} as const
-
-function myFunction(a :'kim') {
-
+// 3. object 자료형 안에 함수를 생성할 수 있음... 타입 지정은 어떻게?
+let info1 = {
+    name : 'kim',
+    plusOne(a :number) :number {
+        return a + 1;
+    },
+    changeName : () => {},
 }
 
-myFunction(content.name); // → 그냥 사용하면 왜 오류가 발생하는가?
-//  3-1. myFunction()의 파라미터는... 'kim'이라는 자료만 들어올 수 있다는 의미가 아니라,
-//       'kim'이라는 타입(string)만 들어올 수 있다는 의미이므로!
-//  3-2. 문제 해결 방법
-//   3-2-1. object 자료형 생성 시 타입 지정을 확실하게
-//   3-2-2. assertion
-//   3-2-3. object 자료형에 as const 사용
+// (참고 1) 콜백함수?
+function func1(a) {
+    a()
+}
+function func2() {
 
-// (참고 2) as const 키워드?
-// 1. 이 object는 literal type 지정을 알아서 해달라는 의미
-//    → object value 값을 그대로 타입으로 지정해줌
-// 2. object 속성들에 모두 readonly 붙여줌
-// 3. object 자료를 매우 엄격하게 관리하고 싶다면 활용
+}
+func1(func2); // func2가 콜백함수
+
+// (참고 2) type alias 연습
+type Member = {
+    name : 'kim',
+    plusOne : (x :number) => number,
+    changeName : () => void,
+}
+let info2 :Member = {
+    name : 'kim',
+    plusOne(a :number) :number {
+        return a + 1;
+    },
+    changeName : () => {},
+}
+
+// ===== //
+
+type FunctionType2 = (str :string) => string
+let cutZero :FunctionType2 = function cutZero(str) {
+    // 특정 문자 제거하기
+    let newStr :string = str.replace(/^0+/, '');
+    return newStr;
+}
+
+let removeDash :FunctionType2 = function removeDash(str) {
+    // 특정 문자 모두 제거하기(정규식 사용)
+    let newStr :string = str.replace(/-/g, ''); // → '/'는 정규식의 시작과 끝을 의미, 'g'는 Global Search(전역탐색)을 의미
+    return newStr;
+}
+
+console.log(cutZero('00123130'));
+console.log(removeDash('----123---zzz------123'));
+
+// ===== //
+
+type MyType = (str :string) => string;
+
+function myFunction2(a :string, func1 :MyType, func2 :MyType) {
+    let result = func1(a);
+    let result2 = func2(result);
+    console.log(result2);
+}
+myFunction2('010-1111-2222', cutZero, removeDash);
