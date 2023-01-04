7ffda498207e9dd97bd120c445438d9a9aa96cac
ktyoung <ktyoung0507@gmail.com>
2023-01-04 21:48:48 +0900

    interface

--- index.js
+++ index.js
@@ -1,57 +1,17 @@
-// class 타입 지정
-var Person = /** @class */ (function () {
-    function Person(myName) {
-        this.age = 20; // → 필드 타입 지정(자동으로 됨)
-        // return 타입 지정할 필요는 없음(항상 object가 복제되므로)
-        this.name = myName;
+// object 타입 지정
+var square1 = { color: 'red', width: 100 };
+var square2 = { color: 'red', width: 100 };
+var student = { name: 'kim' };
+var teacher = { name: 'kim', age: 40 };
+var cat = { name: "cat", age: 5 };
+var product = { brand: 'Samsung', serialNumber: 1360, model: ['TV', 'phone'] };
+var cart = [{ product: '청소기', price: 7000 }, { product: '삼다수', price: 800 }];
+var newCart = { product: '청소기', price: 7000, card: false };
+var obj = {
+    plus: function (x, y) {
+        return x + y;
+    },
+    minus: function (x, y) {
+        return x - y;
     }
-    // prototype 함수
-    Person.prototype.myFunction = function (a) {
-        console.log("안녕하세요, " + a);
-    };
-    return Person;
-}());
-var person1 = new Person("kim");
-var person2 = new Person("lee");
-console.log(person1);
-console.log(person2);
-person1.myFunction("kim");
-person2.myFunction("lee");
-// (참고 1) class 타입 지정 연습
-var Car = /** @class */ (function () {
-    function Car(model, price) {
-        this.model = model;
-        this.price = price;
-    }
-    Car.prototype.tax = function () {
-        return this.price / 10;
-    };
-    return Car;
-}());
-var car1 = new Car("소나타", 3000);
-console.log(car1);
-console.log(car1.tax());
-var Word = /** @class */ (function () {
-    function Word() {
-        var param = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            param[_i] = arguments[_i];
-        }
-        var nums = [];
-        var strs = [];
-        for (var i = 0; i < param.length; i++) {
-            if (typeof param[i] === "number") {
-                nums.push(param[i]);
-            }
-            else {
-                strs.push(param[i]);
-            }
-        }
-        this.num = nums;
-        this.str = strs;
-    }
-    return Word;
-}());
-var obj = new Word('kim', 3, 5, 'park');
-console.log(obj.num);
-console.log(obj.str);
+};
--- index.ts
+++ index.ts
@@ -1,70 +1,98 @@
-// class 타입 지정
+// object 타입 지정
 
-class Person {
-    age :number = 20; // → 필드 타입 지정(자동으로 됨)
+// type alias 사용
+type SquareType1 = { color :string, width : number }
+let square1 :SquareType1 = { color : 'red', width : 100 }
 
-    // typescript constructor()는 필드값에 변수가 있어야 this.변수 사용 가능
-    name :string;
-    constructor(myName :string) { // → constructor 파라미터 타입 지정도 가능, 
-                                  // return 타입 지정할 필요는 없음(항상 object가 복제되므로)
-        this.name = myName;
-    }
+// interface 사용
+interface SquareType2 { 
+    color :string, 
+    width : number
+}
+let square2 :SquareType2 = { color : 'red', width : 100 }
 
-    // prototype 함수
-    myFunction(a :string) :void {
-        console.log("안녕하세요, " + a);
-    }
+// 1. interface의 장점? extends로 복사 가능
+interface StudentType { 
+    name :string 
+}
+// StudentType의 type을 사용할 수 있음
+interface TeacherType extends StudentType { 
+    age :number 
 }
 
-let person1 = new Person("kim");
-let person2 = new Person("lee");
+let student :StudentType = { name : 'kim' }
+let teacher :TeacherType = { name : 'kim', age : 40 }
 
-console.log(person1);
-console.log(person2);
+// 2. & 기호도 유사한 기능을 만들 수 있음(intersection type)
+//  2-1. extends와 달리 두 타입을 모두 만족하는 타입을 생성 (interface는 타입을 복사해줌)
+type Animal = { name :string }
+type Cat = { age :number } & Animal
 
-person1.myFunction("kim");
-person2.myFunction("lee");
+let cat :Cat = { name : "cat", age : 5 }
 
-// (참고 1) class 타입 지정 연습
-class Car {
-    model :string;
-    price :number;
+// (참고 1) type과 interface의 차이점
+// 1. interface는 중복 선언 가능 (유연함)
+interface Interface {}
+interface Interface {}
+interface Interface {}
+// 2. type은 중복 선언 불가능 (엄격함)
+type MyType = string;
+// type MyType = string;
+// type MyType = string;
 
-    constructor(model :string, price :number) {
-        this.model = model;
-        this.price = price;
-    }
+// (참고 2) interface의 장점
+// 1. 외부 라이브러리의 경우 interface를 많이 사용 → 커스터마이징이 매우 쉬움
+//    다른 사람이 많이 이용할 것 같다면, object 타입을 정할 때 interface 사용
 
-    tax() :number {
-        return this.price / 10;
+// (참고 3) 중복 속성 발생 시
+// 1. extends 사용할 때 interface는 에러로 잡아줌
+interface MyInterface1 {
+    name :string
+}
+// interface MyInterface2 extends MyInterface1 {
+//     name :number
+//     age :number
+// }
+
+// 2. & 기호에서는 에러가 발생하지 않음! → 변수에서 사용할 때 에러로 잡아줌
+type Type1 = { name : string };
+type Type2 = { name : number } & Type1;
+// let var1 :Type = { name : 'kim'};
+
+// (참고 4) interface 연습
+interface ProductType {
+    brand :string, 
+    serialNumber :number, 
+    model :string[]
+}
+let product :ProductType = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
+
+
+
+interface CartType {
+    product :string,
+    price :number
+}
+let cart :CartType[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
+
+
+
+interface NewType extends CartType {
+    card :boolean
+}
+let newCart :NewType = { product : '청소기', price : 7000, card : false }
+
+
+
+interface MathType {
+    plus(x :number, y :number) :number,
+    minus(x :number, y :number) :number
+}
+let obj :MathType = {
+    plus(x, y) {
+        return x + y;
+    },
+    minus(x, y) {
+        return x - y;
     }
 }
-
-let car1 = new Car("소나타", 3000);
-console.log(car1);
-console.log(car1.tax());
-
-class Word {
-    num;
-    str;
-
-    constructor(...param) {
-        let nums :number[] = [];
-        let strs :string[] = [];
-
-        for (let i = 0; i < param.length; i++) {
-            if (typeof param[i] === "number") {
-                nums.push(param[i]);
-            } else {
-                strs.push(param[i]);
-            }
-        }
-
-        this.num = nums;
-        this.str = strs;
-    }
-}
-
-let obj = new Word('kim', 3, 5, 'park');
-console.log(obj.num);
-console.log(obj.str);
