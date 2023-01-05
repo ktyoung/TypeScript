7afa3db01414b552732eea9d5405e93e630f9a45
ktyoung <ktyoung0507@gmail.com>
2023-01-05 17:09:20 +0900

    rest 파라미터, destructuring

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
-// object 타입 지정
-var square1 = { color: 'red', width: 100 };
-var square2 = { color: 'red', width: 100 };
-var student = { name: 'kim' };
-var teacher = { name: 'kim', age: 40 };
-var cat = { name: "cat", age: 5 };
-var product = { brand: 'Samsung', serialNumber: 1360, model: ['TV', 'phone'] };
-var cart = [{ product: '청소기', price: 7000 }, { product: '삼다수', price: 800 }];
-var newCart = { product: '청소기', price: 7000, card: false };
-var obj = {
-    plus: function (x, y) {
-        return x + y;
-    },
-    minus: function (x, y) {
-        return x - y;
+// 함수 rest 파라미터, destructuring 할 때 타입 지정
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
     }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
+// 1. rest parameter?
+//  1-1. 몇 개의 파라미터가 들어올 지 모를 때 사용
+//  1-2. 변수명 앞에 '...' 붙이면 됨
+//  1-3. rest 파라미터는 다른 일반 파라미터 뒤에만 올 수 있음
+//  1-4. rest 파라미터 자리에 집어넣은 값들은 전부 [ ] 안에 담김
+//       → 따라서, 타입 지정은 array 타입 지정처럼 해야함
+function func1( /*num,*/) {
+    var param = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        param[_i] = arguments[_i];
+    }
+    console.log(param);
+}
+func1(1, 2, 3, 4, 5, 6);
+// (참고 1) ... spread operator 상식
+// 괄호를 제거하는 문법
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5];
+var arr3 = __spreadArray(__spreadArray([], arr1, true), arr2, true);
+console.log(arr3); // → [1, 2, 3, 4, 5] 출력
+// (참고 2) destructuring 개념
+// array, object 안에 있는 데이터를 변수로 만들고 싶을 때 사용하는 문법
+var _a = ["hi", 100], var1 = _a[0], var2 = _a[1];
+var _b = { student: true, age: 20 }, student = _b.student, age = _b.age;
+// 2. 함수 파라미터에 destructuring
+//  2-1. 항상 같은 모습의 object, array 자료가 들어올 때 쓰는 문법
+function func2(_a) {
+    var student = _a.student, age = _a.age;
+    console.log(student, age);
+}
+func2({ student: true, age: 20 });
+// (참고 3) rest 파라미터, destructuring 활용 연습
+function maximum() {
+    var num = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        num[_i] = arguments[_i];
+    }
+    var max = 0;
+    for (var i = 0; i < num.length; i++) {
+        if (num[i] > num[i + 1]) {
+            max = num[i];
+        }
+    }
+    return max;
+}
+console.log(maximum(6, 3, 7, 2, 5, 100, 20));
+function objFunction(_a) {
+    var user = _a.user, comment = _a.comment, admin = _a.admin;
+    console.log(user, comment, admin);
+}
+objFunction({ user: 'kim', comment: [3, 5, 4], admin: false });
+function arrFunction(_a) {
+    var a = _a[0], b = _a[1], c = _a[2];
+    console.log(a, b, c);
+}
+arrFunction([40, 'wine', false]);
--- index.ts
+++ index.ts
@@ -1,98 +1,63 @@
-// object 타입 지정
+// 함수 rest 파라미터, destructuring 할 때 타입 지정
 
-// type alias 사용
-type SquareType1 = { color :string, width : number }
-let square1 :SquareType1 = { color : 'red', width : 100 }
-
-// interface 사용
-interface SquareType2 { 
-    color :string, 
-    width : number
+// 1. rest parameter?
+//  1-1. 몇 개의 파라미터가 들어올 지 모를 때 사용
+//  1-2. 변수명 앞에 '...' 붙이면 됨
+//  1-3. rest 파라미터는 다른 일반 파라미터 뒤에만 올 수 있음
+//  1-4. rest 파라미터 자리에 집어넣은 값들은 전부 [ ] 안에 담김
+//       → 따라서, 타입 지정은 array 타입 지정처럼 해야함
+function func1(/*num,*/ ...param :number[]) {
+    console.log(param);
 }
-let square2 :SquareType2 = { color : 'red', width : 100 }
+func1(1,2,3,4,5,6);
 
-// 1. interface의 장점? extends로 복사 가능
-interface StudentType { 
-    name :string 
+// (참고 1) ... spread operator 상식
+// 괄호를 제거하는 문법
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5];
+let arr3 = [...arr1, ...arr2];
+console.log(arr3); // → [1, 2, 3, 4, 5] 출력
+
+// (참고 2) destructuring 개념
+// array, object 안에 있는 데이터를 변수로 만들고 싶을 때 사용하는 문법
+let [var1, var2] = ["hi", 100];
+let { student, age } :{student :boolean, age :number}  = { student : true, age : 20 };
+
+// 2. 함수 파라미터에 destructuring
+//  2-1. 항상 같은 모습의 object, array 자료가 들어올 때 쓰는 문법
+function func2({student, age}) {
+    console.log(student, age);
 }
-// StudentType의 type을 사용할 수 있음
-interface TeacherType extends StudentType { 
-    age :number 
-}
+func2({ student : true, age : 20 });
 
-let student :StudentType = { name : 'kim' }
-let teacher :TeacherType = { name : 'kim', age : 40 }
-
-// 2. & 기호도 유사한 기능을 만들 수 있음(intersection type)
-//  2-1. extends와 달리 두 타입을 모두 만족하는 타입을 생성 (interface는 타입을 복사해줌)
-type Animal = { name :string }
-type Cat = { age :number } & Animal
-
-let cat :Cat = { name : "cat", age : 5 }
-
-// (참고 1) type과 interface의 차이점
-// 1. interface는 중복 선언 가능 (유연함)
-interface Interface {}
-interface Interface {}
-interface Interface {}
-// 2. type은 중복 선언 불가능 (엄격함)
-type MyType = string;
-// type MyType = string;
-// type MyType = string;
-
-// (참고 2) interface의 장점
-// 1. 외부 라이브러리의 경우 interface를 많이 사용 → 커스터마이징이 매우 쉬움
-//    다른 사람이 많이 이용할 것 같다면, object 타입을 정할 때 interface 사용
-
-// (참고 3) 중복 속성 발생 시
-// 1. extends 사용할 때 interface는 에러로 잡아줌
-interface MyInterface1 {
-    name :string
-}
-// interface MyInterface2 extends MyInterface1 {
-//     name :number
-//     age :number
-// }
-
-// 2. & 기호에서는 에러가 발생하지 않음! → 변수에서 사용할 때 에러로 잡아줌
-type Type1 = { name : string };
-type Type2 = { name : number } & Type1;
-// let var1 :Type = { name : 'kim'};
-
-// (참고 4) interface 연습
-interface ProductType {
-    brand :string, 
-    serialNumber :number, 
-    model :string[]
-}
-let product :ProductType = { brand : 'Samsung', serialNumber : 1360, model : ['TV', 'phone'] }
-
-
-
-interface CartType {
-    product :string,
-    price :number
-}
-let cart :CartType[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 } ]
-
-
-
-interface NewType extends CartType {
-    card :boolean
-}
-let newCart :NewType = { product : '청소기', price : 7000, card : false }
-
-
-
-interface MathType {
-    plus(x :number, y :number) :number,
-    minus(x :number, y :number) :number
-}
-let obj :MathType = {
-    plus(x, y) {
-        return x + y;
-    },
-    minus(x, y) {
-        return x - y;
+// (참고 3) rest 파라미터, destructuring 활용 연습
+function maximum(...num :number[]) {
+    let max :number = 0;
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] > num[i+1]) {
+            max = num[i];
+        }
     }
+    return max;
 }
+console.log(maximum(6,3,7,2,5,100,20));
+
+
+
+type ObjType = {
+    user :string,
+    comment :number[],
+    admin :boolean
+}
+function objFunction({user, comment, admin} :ObjType) :void {
+    console.log(user, comment, admin);
+}
+objFunction({ user : 'kim', comment : [3,5,4], admin : false });
+
+
+
+type ArrType = (number | string | boolean)[];
+function arrFunction([a, b, c] :ArrType) {
+    console.log(a, b, c);
+}
+arrFunction([40, 'wine', false]);
