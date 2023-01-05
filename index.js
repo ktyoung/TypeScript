f4691cbcb5e3ab25541d898a41f33fb12b0bec81
ktyoung <ktyoung0507@gmail.com>
2023-01-05 17:29:49 +0900

    Type Narrowing 2

--- index.js
+++ index.js
@@ -1,66 +1,36 @@
-// 함수 rest 파라미터, destructuring 할 때 타입 지정
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
+// Type Narrowing 2
+// 1. && 연산자로 null & undefined 타입 체크
+//  1-1. 변수가 undefined라면 undefined가 남아서 if문이 실행되지 않고, (조건식에 falsy 값이 남으므로)
+//  1-2. 변수가 string 타입이면 if문이 실행
+//  1-3. 변수가 null, undefined인 경우를 쉽게 거를 수 있는 문법
+//  1-4. 굳이 사용하지 않아도 되는 문법... if/else 문법이 더 명확함
+function func1(a) {
+    if (a && typeof a === 'string') {
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-// 1. rest parameter?
-//  1-1. 몇 개의 파라미터가 들어올 지 모를 때 사용
-//  1-2. 변수명 앞에 '...' 붙이면 됨
-//  1-3. rest 파라미터는 다른 일반 파라미터 뒤에만 올 수 있음
-//  1-4. rest 파라미터 자리에 집어넣은 값들은 전부 [ ] 안에 담김
-//       → 따라서, 타입 지정은 array 타입 지정처럼 해야함
-function func1( /*num,*/) {
-    var param = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        param[_i] = arguments[_i];
+}
+function func2(animal) {
+    // Fish 타입 검사
+    if ('swim' in animal) {
+        animal.swim;
     }
-    console.log(param);
 }
-func1(1, 2, 3, 4, 5, 6);
-// (참고 1) ... spread operator 상식
-// 괄호를 제거하는 문법
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5];
-var arr3 = __spreadArray(__spreadArray([], arr1, true), arr2, true);
-console.log(arr3); // → [1, 2, 3, 4, 5] 출력
-// (참고 2) destructuring 개념
-// array, object 안에 있는 데이터를 변수로 만들고 싶을 때 사용하는 문법
-var _a = ["hi", 100], var1 = _a[0], var2 = _a[1];
-var _b = { student: true, age: 20 }, student = _b.student, age = _b.age;
-// 2. 함수 파라미터에 destructuring
-//  2-1. 항상 같은 모습의 object, array 자료가 들어올 때 쓰는 문법
-function func2(_a) {
-    var student = _a.student, age = _a.age;
-    console.log(student, age);
+// 3. instanceof 연산자로 object narrowing
+var date = new Date();
+if (date instanceof Date) {
 }
-func2({ student: true, age: 20 });
-// (참고 3) rest 파라미터, destructuring 활용 연습
-function maximum() {
-    var num = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        num[_i] = arguments[_i];
+// in(배타적인 속성 없음), instanceof(부모 class 없음) 키워드 사용 불가
+function func3(x) {
+    if (x.wheel === '4개') {
+        console.log("Car 타입입니다.");
     }
-    var max = 0;
-    for (var i = 0; i < num.length; i++) {
-        if (num[i] > num[i + 1]) {
-            max = num[i];
-        }
+    else {
+        console.log("Bike 타입입니다.");
     }
-    return max;
 }
-console.log(maximum(6, 3, 7, 2, 5, 100, 20));
-function objFunction(_a) {
-    var user = _a.user, comment = _a.comment, admin = _a.admin;
-    console.log(user, comment, admin);
-}
-objFunction({ user: 'kim', comment: [3, 5, 4], admin: false });
-function arrFunction(_a) {
-    var a = _a[0], b = _a[1], c = _a[2];
-    console.log(a, b, c);
-}
-arrFunction([40, 'wine', false]);
+func3({ wheel: "2개", color: "red" });
+// (참고 1) && 연산자의 다른 기능
+// 1. && 기호로 비교할 때 true와 false를 넣는게 아니라 자료형을 넣으면
+// 2. && 사이에서 처음 등장하는 falsy 값을 찾아주고, 그게 아니면 마지막 값을 남겨줌
+// 3. falsy 값은 false와 유사한 기능을 하는 null, undefined, NaN 값들을 의미
+1 && null && 3; // null
+undefined && '안녕' && 100; // undefined
--- index.ts
+++ index.ts
@@ -1,63 +1,58 @@
-// 함수 rest 파라미터, destructuring 할 때 타입 지정
+// Type Narrowing 2
 
-// 1. rest parameter?
-//  1-1. 몇 개의 파라미터가 들어올 지 모를 때 사용
-//  1-2. 변수명 앞에 '...' 붙이면 됨
-//  1-3. rest 파라미터는 다른 일반 파라미터 뒤에만 올 수 있음
-//  1-4. rest 파라미터 자리에 집어넣은 값들은 전부 [ ] 안에 담김
-//       → 따라서, 타입 지정은 array 타입 지정처럼 해야함
-function func1(/*num,*/ ...param :number[]) {
-    console.log(param);
-}
-func1(1,2,3,4,5,6);
+// 1. && 연산자로 null & undefined 타입 체크
+//  1-1. 변수가 undefined라면 undefined가 남아서 if문이 실행되지 않고, (조건식에 falsy 값이 남으므로)
+//  1-2. 변수가 string 타입이면 if문이 실행
+//  1-3. 변수가 null, undefined인 경우를 쉽게 거를 수 있는 문법
+//  1-4. 굳이 사용하지 않아도 되는 문법... if/else 문법이 더 명확함
+function func1(a :string | undefined) {
+    if (a && typeof a === 'string') {
 
-// (참고 1) ... spread operator 상식
-// 괄호를 제거하는 문법
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5];
-let arr3 = [...arr1, ...arr2];
-console.log(arr3); // → [1, 2, 3, 4, 5] 출력
-
-// (참고 2) destructuring 개념
-// array, object 안에 있는 데이터를 변수로 만들고 싶을 때 사용하는 문법
-let [var1, var2] = ["hi", 100];
-let { student, age } :{student :boolean, age :number}  = { student : true, age : 20 };
-
-// 2. 함수 파라미터에 destructuring
-//  2-1. 항상 같은 모습의 object, array 자료가 들어올 때 쓰는 문법
-function func2({student, age}) {
-    console.log(student, age);
-}
-func2({ student : true, age : 20 });
-
-// (참고 3) rest 파라미터, destructuring 활용 연습
-function maximum(...num :number[]) {
-    let max :number = 0;
-    for (let i = 0; i < num.length; i++) {
-        if (num[i] > num[i+1]) {
-            max = num[i];
-        }
     }
-    return max;
 }
-console.log(maximum(6,3,7,2,5,100,20));
 
-
-
-type ObjType = {
-    user :string,
-    comment :number[],
-    admin :boolean
+// 2. in 키워드로 object narrowing
+//  2-1. typeof 연산자는 사용자 정의 타입을 판정할 수 없음. (number string boolean... 등의 방식으로만 판정 가능)
+//  2-2. 속성명 in 오브젝트 자료
+//  2-3. 서로 가진 속성명(배타적인 속성)이 다르면 in 키워드 사용
+type Fish = { swim :string };
+type Bird = { fly :string };
+function func2(animal :Fish | Bird) {
+    // Fish 타입 검사
+    if ('swim' in animal) {
+        animal.swim;
+    }
 }
-function objFunction({user, comment, admin} :ObjType) :void {
-    console.log(user, comment, admin);
+
+// 3. instanceof 연산자로 object narrowing
+let date = new Date();
+if (date instanceof Date) {
+
 }
-objFunction({ user : 'kim', comment : [3,5,4], admin : false });
 
-
-
-type ArrType = (number | string | boolean)[];
-function arrFunction([a, b, c] :ArrType) {
-    console.log(a, b, c);
+// 4. object 타입이 비슷하게 생겼을 때 narrowing
+//  4-1. literal type 만들어두면 narrowing이 편리해짐
+type Car = {
+    wheel :'4개',
+    color :string
 }
-arrFunction([40, 'wine', false]);
+type Bike = {
+    wheel :'2개',
+    color :string
+}
+// in(배타적인 속성 없음), instanceof(부모 class 없음) 키워드 사용 불가
+function func3(x :Car | Bike) {
+    if (x.wheel === '4개') {
+        console.log("Car 타입입니다.")
+    } else {
+        console.log("Bike 타입입니다.")
+    }
+}
+func3({ wheel : "2개", color : "red" });
+
+// (참고 1) && 연산자의 다른 기능
+// 1. && 기호로 비교할 때 true와 false를 넣는게 아니라 자료형을 넣으면
+// 2. && 사이에서 처음 등장하는 falsy 값을 찾아주고, 그게 아니면 마지막 값을 남겨줌
+// 3. falsy 값은 false와 유사한 기능을 하는 null, undefined, NaN 값들을 의미
+1 && null && 3   // null
+undefined && '안녕' && 100  // undefined
