e448269968aedb079f9391e874375cb9210ddfce
ktyoung <ktyoung0507@gmail.com>
2023-01-05 17:56:04 +0900

    public, private

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-// Never Type
-// 1. function return 값에 붙일 수 있는 never type
-//  1-1. return 값이 없어야 함
-//  1-2. endpoint가 없어야 함(함수 실행이 끝나지 않아야 함)
-function func1() {
-    // 1-3. 함수 실행이 끝나지 않는 경우?
-    // 1-3-1. 에러 발생
-    throw new Error(); // → 강제 에러 발생... 코드 실행 중단됨
-    // 1-3-2. 무한 반복
-    while (true) { /*...*/ }
-    // return undefined; → 함수는 기본적으로 이 return 값을 가지고 있다.
-}
-// 2. never 타입 사용 방법?
-//  2-1. 대체로 쓸 곳이 없음. void 타입으로 대체 가능하기 때문.
-// 3. never 타입은 코드를 이상하게 구현하면 출몰
-//  3-1. 이상한 narrowing
-function func2(parameter) {
-    if (typeof parameter === "string") {
-        console.log(parameter);
+// public, private
+// 1. public 키워드
+var User1 = /** @class */ (function () {
+    function User1(str) {
+        this.name = str;
     }
-    else {
-        // 굳이 할 필요 없는 narrowing
-        console.log(parameter); // → parameter의 타입이 never가 됨
+    // 1-3. 함수에도 public 키워드 사용 가능
+    User1.prototype.myFunction = function () {
+    };
+    return User1;
+}());
+var user_1 = new User1('park');
+user_1.name = "park";
+// 2. private 키워드
+var User2 = /** @class */ (function () {
+    function User2(str) {
+        this.familyName = "Kim"; // → familyName은 사용자가 임의로 변경하면 안 되는 변수
+        this.name = this.familyName + str;
     }
-}
-//  3-2. 어떤 함수 표현식은 return 타입이 자동으로 never
-var func3 = function () {
-    throw new Error();
-};
+    //  2-2. 자식들이 familyName을 바꾸고 싶으면? → 수정 함수 작성 후 자식들이 사용
+    User2.prototype.changeName = function () {
+        this.familyName = "park";
+    };
+    return User2;
+}());
+var user_2 = new User2('Minsu');
+console.log(user_2);
+// console.log(user_2.familyName); → familyName 속성 사용 불가
+user_2.changeName(); // → 수정 함수로 private 속성인 familyName을 수정함
+console.log(user_2);
+// 3. public 키워드 쓰면 this 키워드 생략 가능
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    return Person;
+}());
+var person = new Person('kim');
+console.log(person);
--- index.ts
+++ index.ts
@@ -1,33 +1,50 @@
-// Never Type
+// public, private
 
-// 1. function return 값에 붙일 수 있는 never type
-//  1-1. return 값이 없어야 함
-//  1-2. endpoint가 없어야 함(함수 실행이 끝나지 않아야 함)
-function func1() :never {
-    // 1-3. 함수 실행이 끝나지 않는 경우?
-    // 1-3-1. 에러 발생
-    throw new Error() // → 강제 에러 발생... 코드 실행 중단됨
-    // 1-3-2. 무한 반복
-    while(true) {/*...*/}
+// 1. public 키워드
+class User1 {
+    //  1-1. public 키워드가 붙으면 모든 자식들이 이용 가능
+    //  1-2. public 키워드는 항상 생략되어 있음
+    public name :string;
 
-    // return undefined; → 함수는 기본적으로 이 return 값을 가지고 있다.
+    constructor(str :string) {
+        this.name = str;
+    }
+    // 1-3. 함수에도 public 키워드 사용 가능
+    public myFunction() {
+
+    }
 }
+let user_1 = new User1('park');
+user_1.name = "park";
 
-// 2. never 타입 사용 방법?
-//  2-1. 대체로 쓸 곳이 없음. void 타입으로 대체 가능하기 때문.
+// 2. private 키워드
+class User2 {
+    //  2-1. private 키워드가 붙으면 class 안에서만 수정, 이용 가능
+    name :string;
+    private familyName :string = "Kim"; // → familyName은 사용자가 임의로 변경하면 안 되는 변수
 
-// 3. never 타입은 코드를 이상하게 구현하면 출몰
-//  3-1. 이상한 narrowing
-function func2(parameter :string) {
-    if (typeof parameter === "string") {
-        console.log(parameter);
-    } else {
-        // 굳이 할 필요 없는 narrowing
-        console.log(parameter); // → parameter의 타입이 never가 됨
+    constructor(str :string) {
+        this.name = this.familyName + str;
+    }
+
+    //  2-2. 자식들이 familyName을 바꾸고 싶으면? → 수정 함수 작성 후 자식들이 사용
+    changeName() {
+        this.familyName = "park";
     }
 }
 
-//  3-2. 어떤 함수 표현식은 return 타입이 자동으로 never
-let func3 = function() { // → func3의 타입이 never가 됨
-    throw new Error()
+let user_2 = new User2('Minsu');
+console.log(user_2);
+// console.log(user_2.familyName); → familyName 속성 사용 불가
+user_2.changeName(); // → 수정 함수로 private 속성인 familyName을 수정함
+console.log(user_2);
+
+// 3. public 키워드 쓰면 this 키워드 생략 가능
+class Person {
+    constructor(public name :string) { // → 여기에 입력된 파라미터는 자식의 name 속성에 기입
+
+    }
 }
+
+let person = new Person('kim');
+console.log(person);
