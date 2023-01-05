8fa6e50310c9663cfeb6f7bcd50c152a59fb79d6
ktyoung <ktyoung0507@gmail.com>
2023-01-05 23:26:21 +0900

    Generic 함수

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-// import export
-// 1. import {변수명} from '경로'
-import { userName, userAge } from "./a";
-console.log(userName, userAge);
-var var1 = 'park';
-// 2. namespace (import export 과거 버전)
-//  2-1. TypeScript 타입 변수를 숨기는 문법
-// (참고 1) import 옛날 방식
-// /// <reference path="./a.ts" />
-// (참고 2) import 연습
-var var3 = { wheel: 4, model: "소나타" };
-var var4 = { wheel: 2, model: "시티" };
-var objFunction = function objFunction(a) {
-    console.log(a);
-};
+// Generic 함수 
+// 1. unknown 값을 리턴하는 함수 만들기
+function func1(arr) {
+    return arr[0]; // → unknown 타입을 리턴함
+}
+var a = func1([10, 20]);
+console.log(a); // → 10 출력... 그러나 타입이 unknown 타입임
+// console.log(a + 1); → unknown 타입과 연산 불가
+// 2. Generic 함수 만들기
+function func2(arr) {
+    return arr[0];
+}
+var b = func2([10, 20]);
+console.log(b + 1);
+//  2-1. Generic 함수의 장점?
+//   2-1-1. 확장성이 좋음 → 매번 다른 타입 출력 가능
+var c = func2(['10', '20']);
+//  2-2. 타입파라미터 제한 (일종의 narrowing)
+//   2-2-1. MyType이 extends 우측에 있는 속성을 가지고 있는지 검사
+function minus(x) {
+    return x - 1; // → 파라미터의 타입을 제한하지 않으면 x의 타입이 불확실하므로 에러가 발생함
+}
+var d = minus(100);
+function func3(x) {
+    return x.length;
+}
+var e = func3(['100']);
+// (참고 1) Generic 함수 연습
+function count(x) {
+    return x.length;
+}
+var str = count('hello');
+var arr = count(['kim', 'park']);
+console.log(str, arr);
+var data = '{"name" : "dog", "age" : 1 }';
+function toObj(x) {
+    return JSON.parse(x);
+}
+var result = toObj(data);
+console.log(result);
+var Person = /** @class */ (function () {
+    function Person(a) {
+        this.name = a;
+    }
+    return Person;
+}());
+var person = new Person('안녕');
+person.name;
--- index.ts
+++ index.ts
@@ -1,21 +1,67 @@
-// import export
+// Generic 함수 
 
-// 1. import {변수명} from '경로'
-import { userName, userAge, Type, Car, Bike, ObjFunction } from "./a";
-
-console.log(userName, userAge);
-let var1 :Type = 'park';
-
-// 2. namespace (import export 과거 버전)
-//  2-1. TypeScript 타입 변수를 숨기는 문법
-
-// (참고 1) import 옛날 방식
-// /// <reference path="./a.ts" />
-
-// (참고 2) import 연습
-let var3 :Car = { wheel : 4, model : "소나타" };
-let var4 :Bike = { wheel : 2, model : "시티" };
-
-let objFunction :ObjFunction = function objFunction(a) {
-    console.log(a);
+// 1. unknown 값을 리턴하는 함수 만들기
+function func1(arr :unknown[]) {
+    return arr[0]; // → unknown 타입을 리턴함
 }
+let a = func1([10, 20]);
+console.log(a); // → 10 출력... 그러나 타입이 unknown 타입임
+// console.log(a + 1); → unknown 타입과 연산 불가
+
+// 2. Generic 함수 만들기
+function func2<MyType>(arr :MyType[]) :MyType {
+    return arr[0];
+}
+let b = func2<number>([10, 20]);
+console.log(b + 1);
+
+//  2-1. Generic 함수의 장점?
+//   2-1-1. 확장성이 좋음 → 매번 다른 타입 출력 가능
+let c = func2<string>(['10', '20']);
+
+//  2-2. 타입파라미터 제한 (일종의 narrowing)
+//   2-2-1. MyType이 extends 우측에 있는 속성을 가지고 있는지 검사
+function minus<MyType extends number>(x :MyType) {
+    return x - 1; // → 파라미터의 타입을 제한하지 않으면 x의 타입이 불확실하므로 에러가 발생함
+}
+let d = minus<number>(100);
+//   2-2-2. 사용자 정의 타입도 extends 가능
+interface LengthCheck {
+    length :number;
+}
+function func3<MyType extends LengthCheck>(x :MyType) {
+    return x.length;
+}
+let e = func3<string[]>(['100']);
+
+// (참고 1) Generic 함수 연습
+function count<MyType extends string | string[]>(x :MyType) {
+    return x.length;
+}
+let str = count<string>('hello');
+let arr = count<string[]>(['kim', 'park']);
+console.log(str, arr);
+
+
+
+let data = '{"name" : "dog", "age" : 1 }';
+interface Animal {
+    name :string,
+    age :number
+}
+function toObj<MyType>(x :string) :MyType {
+    return JSON.parse(x);
+}
+let result = toObj<Animal>(data);
+console.log(result);
+
+
+
+class Person<MyType> {
+    name;
+    constructor(a :MyType) {
+        this.name = a;
+    }
+}
+let person = new Person<string>('안녕');
+person.name;
