55e1f0b7ec5779f10ab4ff41e352e19120e6e32e
ktyoung <ktyoung0507@gmail.com>
2023-01-05 18:48:22 +0900

    protected, static

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
-// public, private
-// 1. public 키워드
-var User1 = /** @class */ (function () {
-    function User1(str) {
-        this.name = str;
-    }
-    // 1-3. 함수에도 public 키워드 사용 가능
-    User1.prototype.myFunction = function () {
+// protected, static
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// 1. protected 키워드
+//  1-1. private 키워드와 유사함 → 클래스 안에서만 사용, 수정 가능
+var User1 = /** @class */ (function () {
+    function User1() {
+        this.x = 10;
+    }
     return User1;
 }());
-var user_1 = new User1('park');
-user_1.name = "park";
-// 2. private 키워드
-var User2 = /** @class */ (function () {
-    function User2(str) {
-        this.familyName = "Kim"; // → familyName은 사용자가 임의로 변경하면 안 되는 변수
-        this.name = this.familyName + str;
+//  1-2. 차이점? 상속된 클래스에서도 사용 가능 (private는 상속해도 사용 불가)
+//  1-3. extends 된 class는 사용 가능, 자식들 사용 불가능
+var NewUser = /** @class */ (function (_super) {
+    __extends(NewUser, _super);
+    function NewUser() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    //  2-2. 자식들이 familyName을 바꾸고 싶으면? → 수정 함수 작성 후 자식들이 사용
-    User2.prototype.changeName = function () {
-        this.familyName = "park";
+    NewUser.prototype.doThis = function () {
+        this.x = 20;
     };
+    return NewUser;
+}(User1));
+// 2. static 키워드
+//  2-1. 부모 class에 직접 부여되는 키워드(자식에게 물려주지 않음) → 부모 클래스에서만 사용 가능
+var User2 = /** @class */ (function () {
+    function User2() {
+    }
+    User2.x = 10;
+    // 2-2. private / protected / public + static 가능
+    User2.y = 20;
     return User2;
 }());
-var user_2 = new User2('Minsu');
-console.log(user_2);
-// console.log(user_2.familyName); → familyName 속성 사용 불가
-user_2.changeName(); // → 수정 함수로 private 속성인 familyName을 수정함
-console.log(user_2);
-// 3. public 키워드 쓰면 this 키워드 생략 가능
-var Person = /** @class */ (function () {
-    function Person(name) {
-        this.name = name;
+var child = new User2();
+// console.log(child.x); → 오류 발생
+console.log(User2.x);
+// 3. 접근제한자 활용 예시
+var User3 = /** @class */ (function () {
+    function User3() {
+        this.intro = User3.skill + ' 전문가입니다.';
     }
-    return Person;
+    User3.skill = 'js';
+    return User3;
 }());
-var person = new Person('kim');
-console.log(person);
+var user_3 = new User3();
+console.log(user_3);
+User3.skill = "ts"; // → 이후 자식들은 ts skill을 가짐
+var user_4 = new User3();
+console.log(user_4);
--- index.ts
+++ index.ts
@@ -1,50 +1,40 @@
-// public, private
+// protected, static
 
-// 1. public 키워드
+// 1. protected 키워드
+//  1-1. private 키워드와 유사함 → 클래스 안에서만 사용, 수정 가능
 class User1 {
-    //  1-1. public 키워드가 붙으면 모든 자식들이 이용 가능
-    //  1-2. public 키워드는 항상 생략되어 있음
-    public name :string;
-
-    constructor(str :string) {
-        this.name = str;
-    }
-    // 1-3. 함수에도 public 키워드 사용 가능
-    public myFunction() {
-
+    protected x :number = 10;
+}
+//  1-2. 차이점? 상속된 클래스에서도 사용 가능 (private는 상속해도 사용 불가)
+//  1-3. extends 된 class는 사용 가능, 자식들 사용 불가능
+class NewUser extends User1 {
+    doThis() {
+        this.x = 20;
     }
 }
-let user_1 = new User1('park');
-user_1.name = "park";
 
-// 2. private 키워드
+// 2. static 키워드
+//  2-1. 부모 class에 직접 부여되는 키워드(자식에게 물려주지 않음) → 부모 클래스에서만 사용 가능
 class User2 {
-    //  2-1. private 키워드가 붙으면 class 안에서만 수정, 이용 가능
-    name :string;
-    private familyName :string = "Kim"; // → familyName은 사용자가 임의로 변경하면 안 되는 변수
-
-    constructor(str :string) {
-        this.name = this.familyName + str;
-    }
-
-    //  2-2. 자식들이 familyName을 바꾸고 싶으면? → 수정 함수 작성 후 자식들이 사용
-    changeName() {
-        this.familyName = "park";
-    }
+    static x :number = 10;
+    // 2-2. private / protected / public + static 가능
+    private static y :number = 20;
 }
 
-let user_2 = new User2('Minsu');
-console.log(user_2);
-// console.log(user_2.familyName); → familyName 속성 사용 불가
-user_2.changeName(); // → 수정 함수로 private 속성인 familyName을 수정함
-console.log(user_2);
+let child = new User2();
+// console.log(child.x); → 오류 발생
+console.log(User2.x);
 
-// 3. public 키워드 쓰면 this 키워드 생략 가능
-class Person {
-    constructor(public name :string) { // → 여기에 입력된 파라미터는 자식의 name 속성에 기입
-
-    }
+// 3. 접근제한자 활용 예시
+class User3 {
+    static skill :string = 'js';
+    intro :string = User3.skill + ' 전문가입니다.';
 }
 
-let person = new Person('kim');
-console.log(person);
+let user_3 = new User3();
+console.log(user_3);
+
+User3.skill = "ts"; // → 이후 자식들은 ts skill을 가짐
+
+let user_4 = new User3();
+console.log(user_4);
