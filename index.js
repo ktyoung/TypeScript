8b18019793e042bfcff50b3f5c4bb23c21b27e2c
ktyoung <ktyoung0507@gmail.com>
2023-01-07 19:19:56 +0900

    조건문 타입 & infer

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// object 타입 변환기
-// 1. keyof 연산자
-//  1-1. JS 방식
-var obj = {
-    name: 'kim',
-    age: 20,
-};
-Object.keys(obj); // → 오브젝트 자료의 key 값을 array에 담아 보여줌 ['name', 'age']
-var var1 = 'name';
-var var2 = "iPhone";
+// 조건문으로 타입만들기 & infer
+var var1; // → string 타입
+var var2; // → unknown 타입
+var var3;
+var var4;
+var age1;
+var age2;
--- index.ts
+++ index.ts
@@ -1,67 +1,47 @@
-// object 타입 변환기
+// 조건문으로 타입만들기 & infer
 
-// 1. keyof 연산자
-//  1-1. JS 방식
-let obj = {
-    name : 'kim',
-    age : 20,
-}
-Object.keys(obj); // → 오브젝트 자료의 key 값을 array에 담아 보여줌 ['name', 'age']
+// 1. 삼항연산자로 타입 만들기
+//  1-1. type if문은 삼항 연산자로
+//  1-2. 조건식은 extends 키워드 사용
+type Age<T> = T extends string ? string : unknown;
+let var1 :Age<string>; // → string 타입
+let var2 :Age<number>; // → unknown 타입
 
-//  1-2. TS 방식
-interface Person {
-    name :string,
-    age :number
-}
-type PersonKeys = keyof Person // → key 값을 Union Type으로 보여줌 'age' | 'name'
-let var1 :PersonKeys = 'name'
+type FirstItem<T> = T extends any[] ? T[0] : any;
+let var3 :FirstItem<string[]>;
+let var4 :FirstItem<number>;
 
-//  1-3. index signature에 keyof 쓰면?
-interface Phone {
-    [key :string] :number
-}
-type PhoneKeys = keyof Phone // → string | number 타입
-let var2 :PhoneKeys = "iPhone"
+// 2. infer 키워드
+//  2-1. 타입을 왼쪽에서 추출하는 기능
+type Person<T> = T extends infer R ? R : unknown;
+//  2-2. T에서 타입을 추출해서 R 변수에 담음
+type NewPerson = Person<string>
 
+// 3. 함수의 return 타입만 추출하려면...
+//  3-1. ReturnType 기본 함수 사용하면 됨
+type NewType = ReturnType< () => number >;
 
+// (참고 1) infer 키워드 예시
+// 1. array 내부의 타입만 추출하기
+//  1-1. string[] extends (infer R)[] 
+//       → R은 string 타입
+type TypeExtraction<T> = T extends (infer R)[] ? R : unknown;
+type ArrType = TypeExtraction<string[]>; // → NewType은 string 타입
 
-// 2. 타입 변환기 (type mapping)
-//  2-1. [ 자유작명 in keyof 타입파라미터 ] :원하는 타입
-type Car = {
-    color :boolean,
-    model :boolean,
-    price :boolean | number
-}
-
-type TypeChanger<MyType> = {
-    [key in keyof MyType] :string
-    // == key in 'color' | 'model' | 'price'
-}
-
-type NewType = TypeChanger<Car>
+// 2. 함수의 return 타입 추출
+//  2-1. 틀린그림찾기처럼 타입을 추출함... infer와 같은 위치에 있는 타입을 R 변수에 담음
+type FuncExtraction<T> = T extends ( () => infer R ) ? R : unknown;
+//  2-2. () => void extends ( () => infer R ) 이므로 같은 위치에 있는 void 타입을 R 변수에 담음
+type FuncType = FuncExtraction< () => void >;
 
 
 
-// (참고 1) Type Mapping 연습
-type Bus = {
-    color :string,
-    model :boolean,
-    price :number
-}
-type BusChanger<MyType> = {
-    [key in keyof MyType] :string | number
-}
-type NewBus = TypeChanger<Bus>
+// (참고 2) 조건문 타입 & infer 활용 연습
+type MyType<T> = T extends [string, ...any] ? T[0] : unknown;
+let age1 :MyType<[string, number]>;
+let age2 :MyType<[boolean, number]>;
 
 
-
-type Student = {
-    name :string,
-    age :number,
-    score :number,
-    subject :string
-}
-type StudentChanger<MyType, T> = {
-    [key in keyof MyType] :T
-}
-type NewStudent = StudentChanger<Student, string[]>
+type TestType<T> = T extends ( (x: infer R) => any ) ? R : any;
+type type1 = TestType<(x :number) => void>;
+type type2 = TestType<(x :string) => void>;
