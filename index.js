494cf5f76f79e91c04448c22287124e8d2bf129c
ktyoung <ktyoung0507@gmail.com>
2023-01-07 18:40:57 +0900

    object 타입 변환기

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-// object index signatures
-//  1-2. 속성 이름이 숫자인 경우에도 사용 가능
-//   1-2-1. 속성 이름이 숫자여도 object에서 문자화가 되기 때문에 에러가 발생하지 않음
-var user = {
-    0: "kim",
-    1: "20",
-    2: "seoul"
+// object 타입 변환기
+// 1. keyof 연산자
+//  1-1. JS 방식
+var obj = {
+    name: 'kim',
+    age: 20,
 };
-var css = {
-    'font-size': {
-        'font-size': {
-            'font-size': 14
-        }
-    }
-};
-var obj1 = {
-    model: 'k5',
-    brand: 'kia',
-    price: 6000,
-    year: 2030,
-    date: '6월',
-    percent: '5%',
-    dealer: '김차장',
-};
-var obj2 = {
-    'font-size': 10,
-    'secondary': {
-        'font-size': 12,
-        'third': {
-            'font-size': 14
-        }
-    }
-};
+Object.keys(obj); // → 오브젝트 자료의 key 값을 array에 담아 보여줌 ['name', 'age']
+var var1 = 'name';
+var var2 = "iPhone";
--- index.ts
+++ index.ts
@@ -1,59 +1,67 @@
-// object index signatures
+// object 타입 변환기
 
-// 1. index signatures
-//  1-1. 모든 속성의 타입을 한 번에 지정 가능
-interface StringOnly {
-    [key :string] :string, // → 모든 문자로 된 속성은 string 타입을 가짐
-    // age :number, // → age 속성은 string 타입이므로 에러 발생
+// 1. keyof 연산자
+//  1-1. JS 방식
+let obj = {
+    name : 'kim',
+    age : 20,
 }
+Object.keys(obj); // → 오브젝트 자료의 key 값을 array에 담아 보여줌 ['name', 'age']
 
-//  1-2. 속성 이름이 숫자인 경우에도 사용 가능
-//   1-2-1. 속성 이름이 숫자여도 object에서 문자화가 되기 때문에 에러가 발생하지 않음
-let user :StringOnly = {
-    0 : "kim",
-    1 : "20",
-    2 : "seoul"
+//  1-2. TS 방식
+interface Person {
+    name :string,
+    age :number
 }
+type PersonKeys = keyof Person // → key 값을 Union Type으로 보여줌 'age' | 'name'
+let var1 :PersonKeys = 'name'
 
-// 2. 중첩 object 자료형 타입 지정은?
-//  2-1. recursive하게 타입 만들기
-interface CssType {
-    'font-size' :CssType | number // → == { 'font-size' : CssType }
+//  1-3. index signature에 keyof 쓰면?
+interface Phone {
+    [key :string] :number
 }
-
-let css :CssType = {
-    'font-size' : {
-        'font-size' : {
-            'font-size' : 14
-        }
-    }
-}
-
-// (참고 1) object index signatures 연습
-interface ObjType1 {
-    [key :string] :string | number
-}
-let obj1 :ObjType1 = {
-    model : 'k5',
-    brand : 'kia',
-    price : 6000,
-    year : 2030,
-    date : '6월',
-    percent : '5%',
-    dealer : '김차장',
-}
+type PhoneKeys = keyof Phone // → string | number 타입
+let var2 :PhoneKeys = "iPhone"
 
 
-interface ObjType2 {
-    [key :string] :number | ObjType2,
-    'font-size' : number
+
+// 2. 타입 변환기 (type mapping)
+//  2-1. [ 자유작명 in keyof 타입파라미터 ] :원하는 타입
+type Car = {
+    color :boolean,
+    model :boolean,
+    price :boolean | number
 }
-let obj2 :ObjType2 = {
-    'font-size' : 10,
-    'secondary' : {
-      'font-size' : 12,
-      'third' : {
-        'font-size' : 14
-      }
-    }
-  }
+
+type TypeChanger<MyType> = {
+    [key in keyof MyType] :string
+    // == key in 'color' | 'model' | 'price'
+}
+
+type NewType = TypeChanger<Car>
+
+
+
+// (참고 1) Type Mapping 연습
+type Bus = {
+    color :string,
+    model :boolean,
+    price :number
+}
+type BusChanger<MyType> = {
+    [key in keyof MyType] :string | number
+}
+type NewBus = TypeChanger<Bus>
+
+
+
+type Student = {
+    name :string,
+    age :number,
+    score :number,
+    subject :string
+}
+type StudentChanger<MyType, T> = {
+    [key in keyof MyType] :T
+}
+type NewStudent = StudentChanger<Student, string[]>
