47fddf828583b2092a3bcea66f2152344109c09b
ktyoung <ktyoung0507@gmail.com>
2023-01-07 18:24:06 +0900

    object index signatures

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-// implements 키워드
-//  2-2. 이 class가 이 interface에 있는 속성을 가지고 있는지 검사
-var Car = /** @class */ (function () {
-    function Car(a) {
-        this.price = 1000;
-        this.model = a;
+// object index signatures
+//  1-2. 속성 이름이 숫자인 경우에도 사용 가능
+//   1-2-1. 속성 이름이 숫자여도 object에서 문자화가 되기 때문에 에러가 발생하지 않음
+var user = {
+    0: "kim",
+    1: "20",
+    2: "seoul"
+};
+var css = {
+    'font-size': {
+        'font-size': {
+            'font-size': 14
+        }
     }
-    return Car;
-}());
-var car = new Car('모닝');
-var MyClass = /** @class */ (function () {
-    function MyClass() {
+};
+var obj1 = {
+    model: 'k5',
+    brand: 'kia',
+    price: 6000,
+    year: 2030,
+    date: '6월',
+    percent: '5%',
+    dealer: '김차장',
+};
+var obj2 = {
+    'font-size': 10,
+    'secondary': {
+        'font-size': 12,
+        'third': {
+            'font-size': 14
+        }
     }
-    MyClass.prototype.tax = function (a) {
-        return a * 0.1;
-    };
-    return MyClass;
-}());
+};
--- index.ts
+++ index.ts
@@ -1,36 +1,59 @@
-// implements 키워드
+// object index signatures
 
-// 1. interface는?
-//  1-1. object 타입 지정할 때 사용
-//  1-2. class 타입을 확인하고 싶을 때도 사용 가능함
-
-// 2. Car class로부터 생산되는 object는 model과 price 속성을 가짐
-//  2-1. class가 model, price 속성을 가지고 있는지 타입으로 확인하려면 implements 키워드 사용
-interface CarType {
-    model :string,
-    price :number
+// 1. index signatures
+//  1-1. 모든 속성의 타입을 한 번에 지정 가능
+interface StringOnly {
+    [key :string] :string, // → 모든 문자로 된 속성은 string 타입을 가짐
+    // age :number, // → age 속성은 string 타입이므로 에러 발생
 }
-//  2-2. 이 class가 이 interface에 있는 속성을 가지고 있는지 검사
-class Car implements CarType {
-    model :string;
-    price :number = 1000;
-    constructor(a :string) {
-        this.model = a;
+
+//  1-2. 속성 이름이 숫자인 경우에도 사용 가능
+//   1-2-1. 속성 이름이 숫자여도 object에서 문자화가 되기 때문에 에러가 발생하지 않음
+let user :StringOnly = {
+    0 : "kim",
+    1 : "20",
+    2 : "seoul"
+}
+
+// 2. 중첩 object 자료형 타입 지정은?
+//  2-1. recursive하게 타입 만들기
+interface CssType {
+    'font-size' :CssType | number // → == { 'font-size' : CssType }
+}
+
+let css :CssType = {
+    'font-size' : {
+        'font-size' : {
+            'font-size' : 14
+        }
     }
 }
-let car = new Car('모닝');
 
-// 3. implements는 타입 지정 문법이 아님!
-//  3-1. interface에 들어있는 속성을 가지고 있는지 검사하라는 의미
-//       → class에 타입을 할당하고 변형시키는 키워드가 아님
-interface MyType {
-    age :number,
-    tax :(price :number) => number;
+// (참고 1) object index signatures 연습
+interface ObjType1 {
+    [key :string] :string | number
+}
+let obj1 :ObjType1 = {
+    model : 'k5',
+    brand : 'kia',
+    price : 6000,
+    year : 2030,
+    date : '6월',
+    percent : '5%',
+    dealer : '김차장',
 }
 
-class MyClass implements MyType {
-    age; // → any 타입
-    tax(a) { // → 파라미터도 any 타입
-        return a * 0.1;
+
+interface ObjType2 {
+    [key :string] :number | ObjType2,
+    'font-size' : number
+}
+let obj2 :ObjType2 = {
+    'font-size' : 10,
+    'secondary' : {
+      'font-size' : 12,
+      'third' : {
+        'font-size' : 14
+      }
     }
-}
+  }
